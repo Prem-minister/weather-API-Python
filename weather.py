6eed9c22e0cc478e3c6d5baa54162c8af7e33ca5
Prem Sharaan <premjr08@gmail.com>
2024-10-30 14:22:39 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,7 @@
 import requests
 
-response = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=4.2105&lon=101.9758&appid=92f67a0ed9dea38cf4be54a417b77fe6')
-print(response.json())
+try:
+  response = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=4.2105&lon=101.9758&appid=92f67a0ed9dea38cf4be54a417b77fe6')
+  print(response.json())
+except:
+  print("No internet")
